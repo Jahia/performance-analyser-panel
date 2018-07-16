a16adf2bb5c2178bcf4f471dda11cbdb9e17e377
Damien <dsaulnier@jahia.com>
2018-07-16 16:59:48 +0200

    Code cleanup

--- src/main/java/org/jahia/modules/pageperformanceanalyser/actions/PathPickerAction.java
+++ src/main/java/org/jahia/modules/pageperformanceanalyser/actions/PathPickerAction.java
@@ -4,7 +4,10 @@ import org.apache.commons.lang.StringUtils;
 import org.jahia.api.Constants;
 import org.jahia.bin.Action;
 import org.jahia.bin.ActionResult;
-import org.jahia.services.content.*;
+import org.jahia.services.content.JCRContentUtils;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.JCRSessionFactory;
+import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.URLResolver;
@@ -14,7 +17,6 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,7 +28,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class PathPickerAction extends Action {
 
     /* the logger for the class. */
-    private static Logger logger = LoggerFactory.getLogger(PathPickerAction.class);
+    private static final Logger logger = LoggerFactory.getLogger(PathPickerAction.class);
 
     @Override
     public ActionResult doExecute(HttpServletRequest req, final RenderContext renderContext, Resource resource, JCRSessionWrapper session, Map<String, List<String>> parameters, URLResolver urlResolver) throws Exception {
@@ -37,25 +39,12 @@ public class PathPickerAction extends Action {
 
         if (path.contains(sitePath)) {
             final JCRNodeWrapper node = session.getNode(path);
-            try {
-                final JSONObject json = (JSONObject) JCRTemplate.getInstance().doExecute(session.getUser(), Constants.LIVE_WORKSPACE, session.getLocale(),new JCRCallback<Object>() {
-                    @Override
-                    public Object doInJCR(JCRSessionWrapper jcrSession) {
-                        try {
-                            return getSitePathJson(jcrSession, node, renderContext, new AtomicBoolean(true), level);
-                        } catch (RepositoryException e) {
-                            return null;
-                        }
-                    }});
-
-                return new ActionResult(HttpServletResponse.SC_OK, null, json);
-
-            } catch (Exception ex) {
-                logger.error("doExecute() PathPickerAction, Error,", ex);
-                return new ActionResult(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            }
+            final JCRSessionWrapper liveSession = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.LIVE_WORKSPACE);
+            final JSONObject json = getSitePathJson(liveSession, node, renderContext, new AtomicBoolean(true), level);
+            return new ActionResult(HttpServletResponse.SC_OK, null, json);
         } else {
-            if(logger.isDebugEnabled()) logger.debug("The path : " + path + "is not part of the site path : " + sitePath);
+            if (logger.isDebugEnabled())
+                logger.debug("The path : " + path + "is not part of the site path : " + sitePath);
             return new ActionResult(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
@@ -84,26 +73,23 @@ public class PathPickerAction extends Action {
      * @return jsonString @String
      * @throws RepositoryException
      */
-    protected JSONObject getSitePathJson(JCRSessionWrapper session,JCRNodeWrapper node, RenderContext renderContext, AtomicBoolean displayable, int level) throws RepositoryException {
+    private JSONObject getSitePathJson(JCRSessionWrapper liveSession, JCRNodeWrapper node, RenderContext renderContext, AtomicBoolean displayable, int level) throws RepositoryException {
         if (level > 0 || level == -1) {
             if (JCRContentUtils.isADisplayableNode(node, renderContext)) {
                 displayable.set(true);
             }
-            JSONObject jsonObject = new JSONObject();
             boolean noSelect = false;
             boolean notPublished = false;
-            try {
-                //Need to verify that the node exists in live mode
-                JCRNodeWrapper node1 = session.getNode(node.getPath());
+            if (liveSession.nodeExists(node.getPath())) {
+                // The fact that the node exists in live is not enough to be available, it may be unpublished
                 if (!JCRContentUtils.isADisplayableNode(node, renderContext)) {
                     noSelect = true;
                 }
-            }catch (PathNotFoundException e){
+            } else {
                 notPublished = true;
                 noSelect = true;
             }
-            jsonObject = getJson(session,node,renderContext,displayable,level,noSelect, notPublished);
-            return jsonObject;
+            return getJson(liveSession, node, renderContext, displayable, level, noSelect, notPublished);
         } else {
             if (JCRContentUtils.isADisplayableNode(node, renderContext)) {
                 displayable.set(true);
@@ -113,22 +99,22 @@ public class PathPickerAction extends Action {
 
     }
 
-    private JSONObject getJson(JCRSessionWrapper session, JCRNodeWrapper node, RenderContext renderContext, AtomicBoolean displayable, int level, boolean noSelect, boolean notPublished){
+    private JSONObject getJson(JCRSessionWrapper liveSession, JCRNodeWrapper node, RenderContext renderContext, AtomicBoolean displayable, int level, boolean noSelect, boolean notPublished) {
         try {
             JSONObject jsonObject = new JSONObject();
-            if(notPublished){
+            if (notPublished) {
                 jsonObject.put("title", replaceSingleQuotes(node.getDisplayableName()) + " ( Not published ) ");
-            }else{
+            } else {
                 jsonObject.put("title", replaceSingleQuotes(node.getDisplayableName()));
             }
 
             jsonObject.put("href", node.getPath());
             jsonObject.put("noSelect", noSelect);
-            if(noSelect){
-                jsonObject.put("folder",true);
+            if (noSelect) {
+                jsonObject.put("folder", true);
             }
             List<JCRNodeWrapper> childNodeList = new ArrayList<JCRNodeWrapper>();
-                /* getting the folder child nodes */
+            /* getting the folder child nodes */
             childNodeList = JCRContentUtils.getChildrenOfType(node, "nt:base");
 
             if (childNodeList.size() > 0) {
@@ -141,7 +127,7 @@ public class PathPickerAction extends Action {
                     AtomicBoolean isChildDisplayed = new AtomicBoolean(false);
                     // We only push the child to the jsonBuilder is possesses a content template
 
-                    JSONObject temp = getSitePathJson(session,childNodeList.get(index), renderContext, isChildDisplayed, level);
+                    JSONObject temp = getSitePathJson(liveSession, childNodeList.get(index), renderContext, isChildDisplayed, level);
                     if (isChildDisplayed.get()) {
 
                         // if the child is displayed, then the parent must be displayed too
@@ -161,7 +147,7 @@ public class PathPickerAction extends Action {
         } catch (JSONException e) {
             logger.error("An error occured while converting the pages hierachy into a JSON object", e);
             return null;
-        } catch (RepositoryException e){
+        } catch (RepositoryException e) {
             logger.error("An error occured ", e);
             return null;
         }
