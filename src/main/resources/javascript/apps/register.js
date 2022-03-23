f51aebe3941950e2e5c01ff0ec67f54de6512af1
dsaulnier <dsaulnier@jahia.com>
2022-03-23 17:53:40 +0100

    Display the site settings panel only for the sites on which the module is enabled

--- src/main/resources/javascript/apps/register.js
+++ src/main/resources/javascript/apps/register.js
@@ -3,8 +3,9 @@ window.jahia.i18n.loadNamespaces('page-performance-analyzer');
 
 window.jahia.uiExtender.registry.add('adminRoute', 'performanceanalyzer', {
     targets: ['administration-sites:10'],
-    requiredPermission: 'siteAdminUsers',
     label: 'page-performance-analyzer:panel-title',
     isSelectable: true,
+    requiredPermission: 'siteAdminUsers',
+    requireModuleInstalledOnSite: 'page-performance-analyzer',
     iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.performanceanalyzer.html'
 });
